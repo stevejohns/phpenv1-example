2f32b1c775aa4cd49b4fc93be95cd55b12e14290
Steve Johns <steve.johns@dds.international>
2016-02-03 15:34:32 +0000

    Added particle validator, db output and css

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+use Particle\Validator\Validator;
+
 require_once '../vendor/autoload.php';
 //echo "Hello World";
 
@@ -15,17 +17,44 @@ $database = new medoo([
 
 $comment = new SitePoint\Comment($database);
 
-$comment->setEmail('steve.johns@dds.international')
-    ->setName('Steve Johns')
-    ->setComment('It works now, oh yes it does!')
-    ->save();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+//    echo "Form was submitted!";
 
 
+$v = new Validator();
+$v->required('name')->lengthBetween(1, 100)->alnum(true);
+$v->required('email')->email()->lengthBetween(5, 255);
+$v->required('comment')->lengthBetween(10, null);
+$result = $v->validate($_POST);
+if ($result->isValid()) {
+    echo "Submission is good!";
+} else {
+    dump($result->getMessages());
+}
+
+try {
+
+    $comment
+        ->setName($_POST['name'])
+        ->setEmail($_POST['email'])
+        ->setComment($_POST['comment'])
+        ->save();
+    header('Location: /');
+    return;
+
+} catch (\Exception $e) {
+
+    die($e->getMessage());
+
+}
+
+}
 
 //dump($database);
-dump($database->error());
+//dump($database->error());
 
 //var_dump($database);
+
 
 ?>
 <!doctype html>
@@ -42,6 +71,7 @@ dump($database->error());
 
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/custom.css">
     <script src="js/vendor/modernizr-2.8.3.min.js"></script>
 </head>
 <body>
@@ -49,6 +79,15 @@ dump($database->error());
 <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade
     your browser</a> to improve your experience.</p>
 <![endif]-->
+
+
+
+<?php foreach ($comment->findAll() as $comment) : ?>
+    <div class="comment">
+        <h3>On <?= $comment->getSubmissionDate() ?>, <?= $comment->getName() ?> wrote:</h3>
+        <p><?= $comment->getComment(); ?></p>
+    </div>
+<?php endforeach; ?>
 
 <!-- Add your site or application content here -->
 <p>
